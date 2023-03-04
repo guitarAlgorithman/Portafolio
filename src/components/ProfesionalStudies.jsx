6b5fd93d89da1927ed3113a129dc629bb7aaf56f
sauriokat <gustavo.henriquez.m@gmail.com>
2023-03-04 17:25:58 -0300

    intento final 49

--- src/components/ProfesionalStudies.jsx
+++ src/components/ProfesionalStudies.jsx
@@ -9,7 +9,7 @@ function ProfesionalStudies() {
         <tr align="left">Industrial Civil Engineer - UDP</tr>
         <tr align="left">Master (c) in Operation Research - U Chile</tr>
         <tr align="left">Bootcamp Fullstack - UDD</tr >
-        <tr align="left">Financial and accouting management Diploma - UAI</tr>
+        <tr align="left">Financial and accounting management Diploma - UAI</tr>
         <tr align="left">Agile Project Management Diploma - EClass</tr>
         <tr align="left">Personal Training- CEFI Chile</tr>
         <tr> </tr>
