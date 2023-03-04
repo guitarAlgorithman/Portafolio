f0bad2309dc5ed75d9b0c80e1d87381d337a7498
sauriokat <gustavo.henriquez.m@gmail.com>
2023-03-04 15:31:49 -0300

    intento final

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<NOMBRE DE USUARIO>/<NOMBRE DE USUARIO>.github.io.git main
 
 # si estás desplegando en https://<NOMBRE DE USUARIO>.github.io/<REPO>
-# git push -f git@github.com:guitarAlgorithman/Ecommerce_frontend.git main:gh-pages
+# git push -f git@github.com:guitarAlgorithman/Portafolio.git main:gh-pages
 
 cd -
